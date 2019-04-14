1a045df7cc7a54dd79022bc98fac2d940050b682
Abhinav <abhinavkumar.sieger@gmail.com>
2019-04-14 16:19:35 +0530

    Data handling completed.

--- data_handling.py
+++ data_handling.py
@@ -3,22 +3,42 @@ import numpy as np
 import sys
 import re
 
-def read_the_file(filepath):
+def get_example_and_label(filepath,split_factor):
     '''
     This function will read the file and retreive the basic dataset
     consisiting of the words and the corresponding sentiment in the
     following structure:
+
+    dateset:
     [
         [sentiment,list of words]   # example1
         [sentiment,list of words]   # example2
+        ...
+        ...
     ]
 
     Also, it will give a list of the bag of words to make a proper
     word vector representation of each of the example.
+
+    word_bag:
+    {
+        word_name1   : [pos_count,neg_count]
+        word_name2   : [pos_count,neg_count]
+        ...
+        ...
+    }
+    The word bag will only add the enteries for the data in training
+    set to estimate the word probability in both positive and negetive
+    sentiment world.
+
+    We will skip if any new words appear in the test dataset at test
+    time. Actually this should be the approach to be followed in
+    real world also.
     '''
     #Initializing the return structures
-    dataset=[]
-    word_bag={}
+    train_dataset=[]
+    test_dataset=[]
+    train_word_bag={}
 
     #Creating the delimiters to split the string
     #we wont remove the ?,! since they could convey certain emotion
@@ -28,14 +48,12 @@ def read_the_file(filepath):
     print("Opening the datafile in read mode")
     total_pos=0
     total_neg=0
+    ex_count=0
     with open(filepath,'r') as fhandle:
-        # i=0
-        # print(fhandle)
         for line in fhandle:
-            # i+=1
-            # print(line)
-            # if(i==10):
-            #     break
+            #Creating a done flag hashing the elements no to count twice
+            done_flag={}
+
             #Splitting the words by the delimiters
             tokens=re.split(delimeter,line)
             #Now retreiving the class type and doc name
@@ -58,45 +76,76 @@ def read_the_file(filepath):
                 if(token!='' and len(token)!=1):
                     new_tokens.append(token)
                     #Hashing or incrementing the word cound to bag
-                    try:
-                        word_bag[token]+=1
-                    except:
-                        word_bag[token]=1
+                    if(ex_count%split_factor!=0):
+                        #Checking if the word occurance is already reflected
+                        try:
+                            #If already counted then leave
+                            _=done_flag[token]
+                        except:
+                            #Otherwise set the done flag and ranp up count
+                            done_flag[token]=True
+                            _fill_word_bag(token,sentiment,train_word_bag)
 
             #Appening them to the example structure
-            dataset.append([int(sentiment=="neg"),new_tokens])
-            if(sentiment=="neg"):
-                total_neg+=1
+            if(ex_count%split_factor==0):
+                test_dataset.append([int(sentiment=="neg"),new_tokens])
             else:
-                total_pos+=1
-            # print(dataset)
-            # print(tokens)
-            # print(word_bag)
-            # sys.exit(0)
+                train_dataset.append([int(sentiment=="neg"),new_tokens])
+                if(sentiment=="neg"):
+                    total_neg+=1
+                else:
+                    total_pos+=1
+
+            #Finally incrementing the example count
+            ex_count+=1
 
         #Finally printing the word bag to see the frequency of each word
         # #Seeing the frequency of the maximum frequcy ones
-        # import operator
-        # sorted_bag=sorted(word_bag.items(),key=operator.itemgetter(1))
-        # total_word=0
-        # for key,value in sorted_bag:
-        #     print("word: {}\t count: {}".format(key,value))
-        #     total_word+=1
+        import operator
+        sorted_bag=sorted(train_word_bag.items(),key=operator.itemgetter(1))
+        total_word=0
+        for key,value in sorted_bag:
+            if(value[0]+value[1]<9):
+                print("word: {}\t count: {}".format(key,value))
+                total_word+=1
+        print("Total less frequent words:",total_word)
 
-        #Printing the stattistics of the dataset
-        #print(dataset)
-        print("\nTotal word Count:",len(word_bag.keys()))
-        print("Total num of pos:{} and neg:{}".format(total_pos,\
-                                                        total_neg))
+        return total_pos,total_neg,train_dataset,train_word_bag,\
+                test_dataset
 
-        return total_pos,total_neg,dataset,word_bag
-
-
-def get_example_and_label():
+def _fill_word_bag(token,sentiment,word_bag):
     '''
-    This function will chop the line read from file and give the
-    appropriate label and the training text (to be encoded later)
+    Helper function to fill the word to the word bag and incrementing the
+    count appropriately either in the positive or the negetive class.
     '''
+    field=int(sentiment=="neg")
+    try:
+        #incrementing the count of the word for its corresponding class
+        word_bag[token][field]+=1
+    except:
+        #Initializing the word entries with zero
+        word_bag[token]=[0,0]
+        word_bag[token][field]+=1
+
+def get_train_test_split(filepath,split_factor=5):
+    '''
+    This function will be the main handler of the dataset creation and
+    splitting, and finally returning the train and validation split.
+
+    split_factor: represent that we add to test_dataset every split
+                    factor example number.
+    '''
+    #Reading the file and getting the train and test split
+    dataset=get_example_and_label(filepath,split_factor)
+
+    #Retreiving the dataset elements
+    print("\nDataset Summary:")
+    print("Total Positive Exmaple(training):",dataset[0])
+    print("Total Negetive Example(training):",dataset[1])
+    print("Total number of train example:",len(dataset[2]))
+    print("Total number of test examples:",len(dataset[4]))
+
+    return dataset
 
 
 if __name__=="__main__":
@@ -104,4 +153,4 @@ if __name__=="__main__":
     filepath="dataset/naive_bayes_data.txt"
 
     #Calling the transformation function
-    read_the_file(filepath)
+    get_train_test_split(filepath,split_factor=5)
--- naive_bayes.py
+++ naive_bayes.py
@@ -3,9 +3,43 @@ import pandas as pd
 
 #Class Definition for the Naive Bayes Classifier
 class NaiveBayes():
+    #Training Attributes: for probability Estimation
+    train_word_bag=None
+    train_dataset=None
+    total_pos=None
+    total_neg=None
 
-    def __init__():
+    #Validation/Testing Attributes
+    test_dataset=None
+
+    #Initialization function
+    def __init__(self,dataset):
         '''
         DESCRIPTION:
             The constructor for this class.
         '''
+        #initializing the training datset
+        self.total_pos=dataset[0]
+        self.total_neg=dataset[1]
+        self.train_dataset=dataset[2]
+        self.train_word_bag=dataset[3]
+
+        #Retreiving the validation datset
+        self.test_dataset=dataset[4]
+
+    #Function to learning the probability of each word in respective world
+    def estimate_word_prob(self):
+        '''
+        1. TRAIN time:
+        This function will estimate the probability of occurance of
+        a word in the world of a particular sentiment using the maximum
+        likeliehood estimate as we did for the bernoulli trial.
+
+        2. TEST time:
+        The estimate will be done only for the data in training set and
+        will not contain the words which might not be in training set.
+        So, we will skip those words from the review assuming they are
+        only specific to that particular class hence having probability
+        in that world as 1.
+        '''
+        
